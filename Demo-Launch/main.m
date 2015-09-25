c5abcec538e4517e9c05eb10347e4776f361fc7d
Sen <slboat@me.com>
2015-09-25 12:36:46 +0800

    init

---
+++ Demo-Launch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo-Launch
+//
+//  Created by Sen on 9/25/15.
+//  Copyright © 2015 SLboat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
